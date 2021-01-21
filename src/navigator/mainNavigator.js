9e086d6f5cc965285b9cea8c6eb81203f332857a
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2021-01-21 13:11:32 -0800

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfileNavigator from '../features/SimpleUserProfile#######/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,7 +14,8 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
-
+UserProfile: { screen: UserProfileNavigator },
+    
     /** new navigators can be added here */
     SplashScreen: {
       screen: SplashScreen
