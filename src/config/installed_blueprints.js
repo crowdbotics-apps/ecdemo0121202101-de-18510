f8ff36a73c0271ace9ad5beb63b89bdfb12be0a6
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2021-01-21 13:12:08 -0800

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,6 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-
+{ name: 'UserProfile#######', human_name: 'User Profile', access_route: 'UserProfile' },
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ];
